287318b2327cb2d6cdad29666015f6b6f08bb5c4
Treizean Hall <87023363+Treizean@users.noreply.github.com>
2024-09-09 13:03:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(projectNumber) {
+    const details = document.getElementById(`details${projectNumber}`);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
